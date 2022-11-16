6890427fc3be0e6562b523923054c016212f405f
JeevankumariChevula <S549475@NWMISSOURI.EDU>
2022-11-16 11:37:56 -0600

    final1

--- routes/Earpods.js
+++ routes/Earpods.js
@@ -9,3 +9,18 @@ router.get('/', Earpods_controlers.Earpods_view_all_Page );
 router.get('/Earpods/:id', Earpods_controlers.Earpods_detail);
 module.exports = router; 
 
+/* GET detail Earpods page */ 
+router.get('/detail', Earpods_controlers.Earpods_view_one_Page); 
+
+/* GET create Earpods page */ 
+router.get('/create', Earpods_controlers.Earpods_create_Page); 
+
+/* GET create update page */ 
+router.get('/update', Earpods_controlers.Earpods_update_Page);
+
+/* GET delete Earpods page */ 
+router.get('/delete', Earpods_controlers.Earpods_delete_Page); 
+ 
+ 
+ 
+ 
